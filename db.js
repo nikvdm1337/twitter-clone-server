6aa7e9459728313f3765e102286717758c1edb7f
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-09 16:37:44 +0700

    connecting to the proper db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/slack', {useNewUrlParser:true}, (err) => {
+mongoose.connect('mongodb://localhost:27017/twitter', {useNewUrlParser:true}, (err) => {
     if (err) {
         console.log("Connection failed: ", err)
     } else {
