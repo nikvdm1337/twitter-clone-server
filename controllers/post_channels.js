3eb89ee296fab37c7f764f39a9454758ad950cc6
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-09 16:37:33 +0700

    routes added, signup controller

---
+++ controllers/post_channels.js
@@ -0,0 +1,9 @@
+const db_channel = require('../models/channel')
+
+module.exports = (req, res) => {
+	db_channel.create(req.body).then((data) => {
+		res.send(data)
+	}).catch((err) => {
+		res.send(err)
+	})
+}
--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 // Config, importing the modules
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+require('dotenv').config()
+const cors = require('cors')
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+app.use(cors())
 
 require('./db')
-app.get('/api/users', require('./controllers/get_users') )
 
-app.listen(2000, (err) => {
+app.get('/api/messages', require('./controllers/get_messages'))
+app.post('/api/messages', require('./controllers/post_messages'))
+
+app.get('/api/channels', require('./controllers/get_channels'))
+app.post('/api/channels', require('./controllers/post_channels'))
+
+app.get('/api/users', require('./controllers/get_user'))
+
+app.post('/api/signup', require('./controllers/post_user'))
+app.post('/api/login', require('./controllers/login'))
+
+
+
+app.listen(process.env.PORT, (err) => {
     if (err) {
     console.log('ERROR', err)
     } else {
-    console.log(`Connected to localghost 2000`)
+    console.log(`Connected to localghost ${process.env.PORT}`)
     }
 })
