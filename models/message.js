cb8b065a3c5563951350e02a7793631fea4cdf5b
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-09 16:36:18 +0700

    message modal for security

---
+++ models/message.js
@@ -0,0 +1,25 @@
+const db = require('../db')
+const mongoose = require('mongoose')
+
+const db_message = db.model('message', {
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'user',
+		required: [true, 'Message Author is required']
+	},
+	date: {
+		type: Date,
+		default: Date.now()
+	},
+	body: {
+		type: String,
+		required: [true, 'Message Body is required']
+	},
+	channel: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'channel',
+		required: [true, 'Message Channel is required']
+	}
+})
+
+module.exports = db_message
