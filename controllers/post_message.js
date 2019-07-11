ff150cdfd6dde7abc4251e39f6c4f9e1959d55ac
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-11 14:48:07 +0700

    showing channel and user by nickname and channelname

--- controllers/post_message.js
+++ controllers/post_message.js
@@ -5,15 +5,18 @@ module.exports = (req, res) => {
 	let token = req.headers.authorization.split(' ')[1]
 	
 	jwt.verify(token, process.env.SECRET, (err, decoded) => {
-		console.log('decoded1', decoded)
 		if (decoded) {
 			console.log('decoded', decoded)
+			console.log('req.body', req.body)
 			req.body.author = decoded._id
 			db_message.create(req.body).then((data) => {
 				db_message.findById(data._id)
 					.populate({
-						path: 'name',
-						select: 'name email'
+						path: 'author',
+						select: 'name'
+					}).populate({
+						path: 'channel',
+						select: 'name'
 					}).then((message) => {
 						res.send(message)
 					}).catch((err) => {
